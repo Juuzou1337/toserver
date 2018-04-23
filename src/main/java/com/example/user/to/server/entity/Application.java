bb06823161a71fd14ae0eb21fe573adbdec77c25
Juuzou1337 <cucx@mail.ru>
2018-04-23 21:40:09 +0300

    Create connection with database and add table application for example.

---
+++ src/main/java/com/example/user/to/server/entity/Application.java
@@ -0,0 +1,93 @@
+package com.example.user.to.server.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "application")
+public class Application {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 2048)
+    private String description;
+
+    @Column(name = "applicationDate", nullable = false)
+    private Date applicationDate;
+
+    @Column(name = "status", nullable = false)
+    private int status;
+
+    @Column(name = "idUser", nullable = false)
+    private int idUser;
+
+    @Column(name = "idPersonal")
+    private int idPersonal;
+
+    public Application() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getApplicationDate() {
+        return applicationDate;
+    }
+
+    public void setApplicationDate(Date applicationDate) {
+        this.applicationDate = applicationDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getIdPersonal() {
+        return idPersonal;
+    }
+
+    public void setIdPersonal(int idPersonal) {
+        this.idPersonal = idPersonal;
+    }
+}
