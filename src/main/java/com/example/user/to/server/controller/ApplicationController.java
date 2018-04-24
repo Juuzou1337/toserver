df85429458fdb5d1477527a3fcb9cc583e66b6b3
Juuzou1337 <cucx@mail.ru>
2018-04-24 16:10:17 +0300

    Create simple REST service

--- src/main/java/com/example/user/to/server/controller/ApplicationController.java
+++ src/main/java/com/example/user/to/server/controller/ApplicationController.java
@@ -1,18 +1,32 @@
 package com.example.user.to.server.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.user.to.server.entity.Application;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/application")
 public class ApplicationController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getApplication(ModelMap model){
-        return "First application";
+    public Application getApplication(){
+        return createMockApp();
+    }
+
+    private Application createMockApp() {
+        Application application = new Application();
+        application.setId(1);
+        application.setTitle("Свет");
+        application.setDescription("Не работают лампочки");
+        application.setApplicationDate(new Date());
+        application.setStatus(1);
+        application.setIdUser(1);
+        return application;
     }
 }
